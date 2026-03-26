bcb73361aacd0ca6a84e8eacc77711df49a1a570
Maxim Laplante <lapm6225@usherbrooke.ca>
2026-03-26 17:44:34 -0400

    gros robot plus pcb

--- ESP32/src/Config.h
+++ ESP32/src/Config.h
@@ -139,10 +139,10 @@
 // Set to true to run only visual tests (detailed output, no pass/fail)
 #define RUN_VISUAL_TESTS false
 // Set to true to run interactive integration test with real motors
-#define RUN_INTERACTIVE_TEST false
+#define RUN_INTERACTIVE_TEST true
 // Set to true to run the simple dynamixel communication test
 #define RUN_DYNAMIXEL_COMM_TEST false
 // Set to true to run the simple effector test
-#define RUN_EFFECTOR_TEST true
+#define RUN_EFFECTOR_TEST false
 
 #endif // CONFIG_H
